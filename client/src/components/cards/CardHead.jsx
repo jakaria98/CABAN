c5dae0a695c07b49752bf8c940a5a0a0e1f9e3df
jakaria98 <jhossain439@gmail.com>
2024-02-10 23:47:31 -0330

    SKED design changed

--- client/src/components/cards/CardHead.jsx
+++ client/src/components/cards/CardHead.jsx
@@ -2,9 +2,12 @@ import { cardStyle } from './cardStyle';
 
 const CardHead = (props) => {
     return (
-        <div className="row mt-5">
+        <div className="row mt-5" style={{ paddingLeft: '3px', paddingRight: '3px' }}>
             <div className="col">
-                <div className="card bg-info-subtle m-3" style={{...cardStyle, height: props.height}}>
+                <div
+                    className="card m-3"
+                    style={{ ...cardStyle, height: props.height, backgroundColor: props.bgColor , border: props.border}}
+                >
                     {props.children}
                 </div>
             </div>
--- client/src/pages/Sked.jsx
+++ client/src/pages/Sked.jsx
@@ -1,8 +1,42 @@
+import PageTitle from '../components/PageTitle';
+import CardHead from '../components/cards/CardHead';
+import CommonCard from '../components/cards/CommonCard';
+import PAXWeight from '../components/cards/PAXWeight';
+
 const Sked = () => {
     return (
-        <div>
-            <h1>Sked</h1>
-        </div>
+        <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <PageTitle title="Sked W&B" />
+            <div className="row row-cols-md-4 d-flex align-items-center justify-content-between mt-5 text-center">
+                <PAXWeight />
+
+                <CardHead height="33rem" bgColor="#f4f4f4" border="none">
+                    <CardHead height="10rem" bgColor="#f4f4f4">
+                        <div className="card-body d-flex align-items-center justify-content-center">
+                            <p className="card-text">Cargo Allocated to flight Pieces and Weight</p>
+                        </div>
+                    </CardHead>
+                    <CardHead height="10rem">
+                        <div className="card-body d-flex align-items-center justify-content-center">
+                            <p className="card-text">Baggage Pieces and Weight</p>
+                        </div>
+                    </CardHead>
+                </CardHead>
+                <CardHead height="33rem" bgColor="#f4f4f4" border="none">
+                    <CardHead height="10rem">
+                        <div className="card-body d-flex align-items-center justify-content-center">
+                            <p className="card-text">Fuel on board</p>
+                        </div>
+                    </CardHead>
+                    <CardHead height="10rem">
+                        <div className="card-body d-flex align-items-center justify-content-center">
+                            <p className="card-text">MTOW ETC</p>
+                        </div>
+                    </CardHead>
+                </CardHead>
+                <CommonCard text="Calculate" />
+            </div>
+        </main>
     );
 };
 export default Sked;
